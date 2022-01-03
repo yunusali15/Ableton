5f50dd1d59029929bcaac27f7a91079e2fc46ef3
Yunus <yunusali150201@gmail.com>
2022-01-04 01:20:53 +0300

    Added Half About Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import logo from './logo.png';
 import { Navbar } from './Navbar';
+import { AboutPage } from './AboutPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar className='Navbar'/>
+      <AboutPage className='AboutPage'/>
     </div>
   );
 }
